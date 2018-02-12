369d21934cd15960b007e0dd9a47c7a2ae4aca24
brianokinyi <brianokinyi.bo@gmail.com>
2018-02-12 22:40:55 +0300

    linked bootstrap

--- csk/frontend/src/app/app.module.ts
+++ csk/frontend/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { WhoComponent } from './who/who.component';
+import { WhatComponent } from './what/what.component';
+import { StoriesComponent } from './stories/stories.component';
+import { PicturesComponent } from './pictures/pictures.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    WhoComponent,
+    WhatComponent,
+    StoriesComponent,
+    PicturesComponent
   ],
   imports: [
     BrowserModule
