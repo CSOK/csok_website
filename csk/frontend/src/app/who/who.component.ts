a9b8f2562a5ddafd2b9491d1bda7e56d4b2a6952
brianokinyi <brianokinyi.bo@gmail.com>
2018-02-12 22:42:31 +0300

    Added who we are component

---
+++ csk/frontend/src/app/who/who.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-who',
+  templateUrl: './who.component.html',
+  styleUrls: ['./who.component.css']
+})
+export class WhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
